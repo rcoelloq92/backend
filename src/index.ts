7dd8cc6aa92088626f5e41a2ddba8edc72e0beaf
rcoelloq92 <ricardocoelloquezada92@gmail.com>
2025-04-07 14:34:52 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,10 @@ app.use(express.json());
 
 app.use('/api', router);
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
 export const api = functions.https.onRequest(app);
